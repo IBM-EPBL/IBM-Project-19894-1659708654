dc5f335d442dbe2d3f52d657ecd01d9f355408a2
Gowtham314 <113576053+Gowtham314@users.noreply.github.com>
2022-11-11 20:42:37 +0530

    Update Python code.py

--- Develop The Python Script/Publish Data To The IBM Cloud/Python code.py
+++ Develop The Python Script/Publish Data To The IBM Cloud/Python code.py
@@ -1,30 +1,44 @@
 import wiotp.sdk.device
 import time
 import random
+import requests
+import urllib.parse
+address= ['Kodambakkam','T.nagar','West mambalam','vadapalani','ekkattuthangal']
 myConfig = { 
     "identity": {
-        "orgId": "hj5fmy",
-        "typeId": "NodeMCU",
-        "deviceId":"12345"
+        "orgId": "dluuhi",
+        "typeId": "SWMS",
+        "deviceId":"6032"
     },
     "auth": {
-        "token": "12345678"
+        "token": "311519106032"
     }
 }
-
 def myCommandCallback(cmd):
     print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
     m=cmd.data['command']
 
+    
 client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
 client.connect()
-
-while True:
-    temp=random.randint(-20,125)
-    hum=random.randint(0,100)
-    myData={'temperature':temp, 'humidity':hum}
-    client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
-    print("Published data Successfully: %s", myData)
-    client.commandCallback = myCommandCallback
-    time.sleep(2)
+    
+#Location=input("enter location: ")
+#while(Location == address0):
+for x in address:
+  url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(x) +'?format=json'
+  response = requests.get(url).json()
+  a = response[1]["lat"]
+  b = response[1]["lon"]
+  bin_stat = random.randint(0,100)
+  In_percent = str(bin_stat)+ "%"
+  myData={'Latitude':a, 'Longitude':b,"Bin Status":In_percent}
+  client.publishEvent(eventId="status", msgFormat="json", data=myData, qos=0, onPublish=None)
+  print("Published data Successfully: ", myData)
+  client.commandCallback = myCommandCallback
+  time.sleep(2)
 client.disconnect()
+
+
+
+
+
